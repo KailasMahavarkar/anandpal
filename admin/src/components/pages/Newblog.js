4f9b18610db734a584a780464ccf16c7148b61ff
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2021-09-15 01:41:55 +0530

    added broken undo

--- admin/src/components/pages/Newblog.js
+++ admin/src/components/pages/Newblog.js
@@ -1,33 +1,67 @@
-import React, { useState, useRef } from "react";
-
+import React, { useState, useRef, useEffect } from "react";
 import editIcon from "../../../src/assets/editIcon.svg";
 import deleteIcon from "../../../src/assets/deleteIcon.svg";
 import createIcon from "../../../src/assets/createIcon.svg";
 import EditorJs from "react-editor-js";
 import { EDITOR_JS_TOOLS } from "./../../tools";
-
-import { useHistory } from "react-router-dom";
+import useUndo from 'use-undo';
 
 const Newblog = (props) => {
+	const [blogTitle, setBlogTitle] = useState("");
+	const [blogAuthor, setBlogAuthor] = useState("");
+	const [blogState, setBlogState] = useState("unpublish");
+	const [blogUndo, setBlogUndo] = useState();
+	const instanceRef = useRef(null);
 
-    const [blogTitle, setBlogTitle] = useState('');
-    const [blogAuthor, setBlogAuthor] = useState('');
-    const instanceRef = useRef(null);
+	const [
+		timeState,
+		{
+			set: setCount,
+			reset: resetCount,
+			undo: undoCount,
+			redo: redoCount,
+			canUndo,
+			canRedo,
+		},
+	] = useUndo(0);
+	const { present: presentCount } = timeState;
+
+	const blogStateHandler = () => {
+		blogState == "publish"
+			? setBlogState("unpublish")
+			: setBlogState("publish");
+		console.log(blogState);
+	};
+
+	// console.log("present -->", timeState);
+
+	const titleChangeHandler = ({ target: { value } }) => {
+		setBlogTitle(value);
+		console.log(blogTitle);
+	};
+	const authorChangeHandler = ({ target: { value } }) => {
+		setBlogAuthor(value);
+		console.log(blogAuthor);
+	};
+
+	const dataSaveHandler = async () => {
+		setCount(presentCount + 1)
+		// const savedData = await instanceRef.current.save();
+	};
+
+	const dataUndoHandler = async () => {
+		// setBlogTitle(timeState.present[presentCount]);
 
 
-    const titleChangeHandler = ({target: {value}}) => {
-        setBlogTitle(value);
-    }
-    const authorChangeHandler = ({target: {value}}) => {
-        setBlogAuthor(value);
-    }
+        console.log(timeState.past)
+	};
 
-    
-    const handleDataSave = async() => {
-        const savedData = await instanceRef.current.save()
-        console.log(savedData)
-    }
-    
+	const dataClearHandler = async () => {
+		await instanceRef.current.clear();
+		setBlogTitle("");
+		setBlogAuthor("");
+        setCount(0);
+	};
 
 	return (
 		<div className="view">
@@ -36,30 +70,48 @@ const Newblog = (props) => {
 					<input
 						type="text"
 						placeholder="Blog Title"
-                        value={blogTitle}
-                        onChange={titleChangeHandler}
-					></input>
-                </div>
-                <div className="options__main options__author">
-                    <input
-						type="text"
-						placeholder="Author Name"
-                        value={blogAuthor}
-                        onChange={authorChangeHandler}
+						value={blogTitle}
+						onChange={titleChangeHandler}
 					></input>
 				</div>
-				<div className="options__save" onClick={handleDataSave}>Save</div>
-				<div className="options__delete">Delete</div>
-				<div className="options__status">Status</div>
+				<div className="options__main options__author">
+					<input
+						type="text"
+						placeholder="Author Name"
+						value={blogAuthor}
+						onChange={authorChangeHandler}
+					></input>
+				</div>
+				<div className="options__clear" onClick={dataClearHandler}>
+					Clear
+				</div>
+				<div
+					className="options__save"
+					onClick={dataSaveHandler}
+				>
+					Save
+				</div>
+				<div
+					className="options__clear"
+					onClick={dataUndoHandler}
+				>
+					Undo
+				</div>
+
+				<div className="options__status" onClick={blogStateHandler}>
+					{blogState}
+				</div>
 			</div>
 
 			<div className="editorjs__wrapper">
 				<div id="editorjs">
-					<EditorJs 
-                    instanceRef={(instance) => (instanceRef.current = instance)}
-                    tools={EDITOR_JS_TOOLS} 
-                    // data={data}
-                    />
+					<EditorJs
+						instanceRef={(instance) =>
+							(instanceRef.current = instance)
+						}
+						tools={EDITOR_JS_TOOLS}
+						// data={data}
+					/>
 				</div>
 			</div>
 		</div>
