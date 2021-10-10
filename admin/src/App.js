cb66796efbcac5909685ce3eded71454d5a5c4a2
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2021-10-11 03:39:21 +0530

    minor changes and bug fixes

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,22 +1,14 @@
-import "../src/css/loop.css";
-import "../src/css/index.css";
-
 import React, { useEffect, useState } from "react";
-
 import Blogpage from "./components/pages/Blogpage";
 import Loginpage from "./components/pages/Loginpage";
 import Editorpage from "./components/pages/Editorpage";
-import x404 from './components/pages/x404'
+import x404 from "./components/pages/x404";
 import { ProtectedRoute } from "./protected.router";
-import Productpage from './components/pages/Productpage';
+import Productpage from "./components/pages/Productpage";
 import TestPage from "./components/pages/TestPage";
-import Notification from './components/pages/Notificationpage';
+import Notification from "./components/pages/Notificationpage";
 
-import {
-	Switch,
-	Route,
-	useHistory,
-} from "react-router-dom";
+import { Switch, Route, useHistory } from "react-router-dom";
 
 import dotenv from "dotenv";
 import NewProduct from "./components/pages/NewProduct";
@@ -26,19 +18,31 @@ function App(props) {
 	return (
 		<div className="App">
 			<Switch>
-                {/* pattern for base route */}
+				{/* pattern for base route */}
 				<Route exact path="/" component={Loginpage} />
-                <Route exact path="/test" component={TestPage}  />
+				<Route exact path="/test" component={TestPage} />
 
-                {/* pattern for protected routes */}
-                <ProtectedRoute exact path="/blogs" component={Blogpage}  />
-                <ProtectedRoute exact path="/blogs/:blogID?" component={Editorpage}  />
-                <ProtectedRoute exact path="/notify" component={Notification}  />
-                <ProtectedRoute exact path="/products" component={Productpage}  />
-                <ProtectedRoute exact path="/products/:productID?" component={NewProduct}  />
+				{/* pattern for protected routes */}
+				<ProtectedRoute exact path="/blogs" component={Blogpage} />
+				<ProtectedRoute
+					exact
+					path="/blogs/:blogID?"
+					component={Editorpage}
+				/>
+				<ProtectedRoute exact path="/notify" component={Notification} />
+				<ProtectedRoute
+					exact
+					path="/products"
+					component={Productpage}
+				/>
+				<ProtectedRoute
+					exact
+					path="/products/:productID?"
+					component={NewProduct}
+				/>
 
-                {/* pattern for unknown routes */}
-                <Route path="*" component={x404} />                    
+				{/* pattern for unknown routes */}
+				<Route path="*" component={x404} />
 			</Switch>
 		</div>
 	);
