f0c7ca123d08973aafbce0ada75a76172fe6ad6b
BraincityOfficial <mewaconite@gmail.com>
2021-11-10 01:14:43 +0530

    minor changes

--- backend/controllers/Auth/adminDelete.js
+++ backend/controllers/Auth/adminDelete.js
@@ -3,8 +3,8 @@ const AdminModel = require("../../models/admin.model");
 const { isEmpty, typeMatch } = require("../../improve/improve");
 
 // importing logger
-const { logger } = require("../../improve/logger") 
-const log = logger(__filename)
+const { logger } = require("../../improve/logger");
+const log = logger(__filename);
 
 /*
     DE -> 1
@@ -14,38 +14,38 @@ const log = logger(__filename)
 
 const adminDelete = async (req, res, next) => {
 	const { username, password } = req.body;
-    if (!typeMatch(username) || !typeMatch(password) ){
-        return res.status(404).json({
-            msg: `arguments apikey, id, password needs to be string type`,
-        });
-    }
+	if (!typeMatch(username) || !typeMatch(password)) {
+		return res.status(404).json({
+			msg: `arguments apikey, id, password needs to be string type`,
+		});
+	}
 
-    
 	try {
-
 		const deletedUser = await AdminModel.deleteOne(
 			{ username: username },
-			{ status: "deleted" },
+			{ status: "deleted" }
 		);
 
 		if (deletedUser.deletedCount !== 1) {
-            return res.status(400).json({
-                msg: `Admin Username ${username} does not exists`,
-            });
-		} 
+			return res.status(400).json({
+				msg: `Admin Username ${username} does not exists`,
+			});
+		}
 
-        return res.status(200).json({
-            msg: `Username ${username} has been deleted`,
-        });
-
-		
+		return res.status(200).json({
+			msg: `Username ${username} has been deleted`,
+		});
 	} catch (error) {
-        const [ERROR, STATUS, MESSAGE] = ['SE_AUTH_ADMIN_DELETE_01', 500,  'Admin Delete Failed']
+		const [ERROR, STATUS, MESSAGE] = [
+			"SE_AUTH_ADMIN_DELETE_01",
+			500,
+			"Admin Delete Failed",
+		];
 
-        log.error(ERROR, STATUS, 'dragon', MESSAGE)
+		log.error(ERROR, STATUS, "dragon", MESSAGE);
 		return res.status(STATUS).json({
 			msg: "Server Error",
-			error: "SE_ADMIN_DELETE_ERROR"
+			error: "SE_ADMIN_DELETE_ERROR",
 		});
 	}
 };
