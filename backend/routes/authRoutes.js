8359f199d59e8ca2ac237dddd475998fc6723351
BraincityOfficial <mewaconite@gmail.com>
2021-11-09 19:28:28 +0530

    bug fix

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,29 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // middlewares
-const _nullCheckRegister = require("../middlewares/auth/_nullCheckRegister")._nullCheckRegister;
-const _nullCheckLogin = require("../middlewares/auth/_nullCheckLogin")._nullCheckLogin;
-const _apiKeyCheck = require("../middlewares/_apiKeyCheck")._apiKeyCheck;
-const _authToken = require("../middlewares/_authToken")._authToken;
+const _nullCheckLogin =
+	require("../middlewares/auth/_nullCheckLogin")._nullCheckLogin;
 
 // controllers
-const adminRegister = require("../controllers/Auth/adminRegister").adminRegister;
 const adminLogin = require("../controllers/Auth/adminLogin").adminLogin;
 const adminLogout = require("../controllers/Auth/adminLogout").adminLogout;
-const adminDelete = require("../controllers/Auth/adminDelete").adminDelete;
-const renewToken  = require("../controllers/Auth/auth").renewtoken;
-const verifyAccess  = require("../controllers/Auth/verfiyAccess").verfiyAccess;
+const renewToken = require("../controllers/Auth/auth").renewtoken;
 
 // routes
-// router.route("/register").post(_nullCheckRegister,_apiKeyCheck, adminRegister);
 router.route("/login").post(_nullCheckLogin, adminLogin);
 router.route("/logout").post(adminLogout);
-// router.route("/delete").delete(_apiKeyCheck, adminDelete);
 router.route("/refresh").post(renewToken);
-// router.route('/verify').post(_authToken, verifyAccess);
-
-
-
 
 module.exports = router;
